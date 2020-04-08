50fa9cd4ba9d5961a2e439883300c4537e66b963
cboselli00 <c.boselli@itsrizzoli.it>
2020-04-08 17:45:07 +0200

    Modifica del file UserController.java

--- StoCamp/src/main/java/it/itsrizzoli/StoCamp/UserController.java
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/UserController.java
@@ -1,5 +1,24 @@
 package it.itsrizzoli.StoCamp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+	
+	@GetMapping("/login")
+	public String login(LoginForm loginForm) {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin(LoginForm loginForm, BindingResult results) {
+		if(results.hasErrors())
+			return "login";
+		return "Home";
+	}
+	
 	
 }
