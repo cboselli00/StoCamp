ad659b6030ab4a2f4ac827090317b886ef7598af
cboselli00 <c.boselli@itsrizzoli.it>
2020-04-08 14:12:54 +0200

    Creazione prima versione vuota del progetto

---
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/StoCampApplication.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.StoCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoCampApplication.class, args);
+	}
+
+}
---
+++ StoCamp/src/test/java/it/itsrizzoli/StoCamp/StoCampApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.StoCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
