34898b5c070fbf919e748d415a51630d3455b825
cboselli00 <c.boselli@itsrizzoli.it>
2020-04-15 11:27:34 +0200

    Carico le classi Evento e Squadra

---
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/Evento.java
@@ -0,0 +1,30 @@
+package it.itsrizzoli.StoCamp;
+
+public class Evento {
+	int id;
+	String nomesquadra1;
+	String nomesquadra2;
+	public int getId() {
+		return id;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomesquadra1() {
+		return nomesquadra1;
+	}
+	public void setNomesquadra1(String nomesquadra1) {
+		this.nomesquadra1 = nomesquadra1;
+	}
+	public String getNomesquadra2() {
+		return nomesquadra2;
+	}
+	public void setNomesquadra2(String nomesquadra2) {
+		this.nomesquadra2 = nomesquadra2;
+	}
+	
+	
+
+}
---
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/Squadra.java
@@ -0,0 +1,24 @@
+package it.itsrizzoli.StoCamp;
+
+public class Squadra {
+	int id;
+	String nome;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
