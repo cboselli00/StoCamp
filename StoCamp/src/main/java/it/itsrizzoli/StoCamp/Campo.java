7b47b4729866e5dc34b210258ba4697f406dc92e
acriscuoli <a.criscuoli@itsrizzoli.it>
2020-04-15 11:31:17 +0200

    Carico la classe Campo

---
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/Campo.java
@@ -0,0 +1,28 @@
+package it.itsrizzoli.StoCamp;
+
+public class Campo {
+	
+	int id;
+	String nome;
+	String indirizzo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+}
