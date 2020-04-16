169d5da07fd6f5ddb814e511b38ad907e93109c6
acriscuoli <a.criscuoli@itsrizzoli.it>
2020-04-16 12:06:22 +0200

    Modifiche a Campo.java

--- StoCamp/src/main/java/it/itsrizzoli/StoCamp/Campo.java
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/Campo.java
@@ -1,10 +1,35 @@
 package it.itsrizzoli.StoCamp;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Campo {
 	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
 	int id;
+	
+	@Size(min=4, max=30, message = "Nome deve esser tra 4 e 30 caratteri")
+    @NotNull(message = "Nome deve esser inserito")
 	String nome;
+	
+	@Size(min=4, max=30, message = "Indirizzo deve esser tra 4 e 15 caratteri")
+    @NotNull(message = "Indirizzo deve esser inserito")
 	String indirizzo;
+	
+	
+	public Campo(int id,
+			@Size(min = 4, max = 30, message = "Nome deve esser tra 4 e 30 caratteri") @NotNull(message = "Nome deve esser inserito") String nome,
+			@Size(min = 4, max = 30, message = "Indirizzo deve esser tra 4 e 15 caratteri") @NotNull(message = "Indirizzo deve esser inserito") String indirizzo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.indirizzo = indirizzo;
+	}
+	
 	
 	public int getId() {
 		return id;
