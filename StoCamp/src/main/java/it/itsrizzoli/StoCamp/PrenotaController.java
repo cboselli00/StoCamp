289ed9fa4c71f2208e5ce93bd6271614bb8da6bc
cboselli00 <c.boselli@itsrizzoli.it>
2020-04-16 11:30:10 +0200

    Carico aggiornamento di PrenotaController.java e Prenotazione.html

--- StoCamp/src/main/java/it/itsrizzoli/StoCamp/PrenotaController.java
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/PrenotaController.java
@@ -2,12 +2,14 @@ package it.itsrizzoli.StoCamp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class PrenotaController {
 	
 	@GetMapping("/Home")
 	public String Home(PrenotazioneForm prenotazioneForm) {
-		return "/Home";
+		return "Home";
 	}
+
 	
 }
