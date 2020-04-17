a4a0c850cc105137bdf52595b7b1cae7eaada219
cboselli00 <c.boselli@itsrizzoli.it>
2020-04-17 10:48:07 +0200

    Modifica al file MyAccountController.java

--- StoCamp/src/main/java/it/itsrizzoli/StoCamp/MyAccountController.java
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/MyAccountController.java
@@ -2,10 +2,12 @@ package it.itsrizzoli.StoCamp;
 
 import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class MyAccountController {
 	
 	@GetMapping("/Home")
