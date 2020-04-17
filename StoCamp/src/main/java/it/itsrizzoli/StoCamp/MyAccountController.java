4ce9dc848aed4b67dc95ea440eb2493a89e0f5f8
MarcoNegri <marconegri2000@gmail.com>
2020-04-17 10:25:33 +0200

    Carico modifiche a myaccountcontroller.java e myaccount.html

--- StoCamp/src/main/java/it/itsrizzoli/StoCamp/MyAccountController.java
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/MyAccountController.java
@@ -1,12 +1,28 @@
 package it.itsrizzoli.StoCamp;
 
+import javax.validation.Valid;
+
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class MyAccountController {
 	
 	@GetMapping("/Home")
 	public String Home() {
 		return "Home";
+	}
+	
+	@GetMapping("/settingsaccount")
+	public String contattaci(SettingsAccountForm settingsAccountForm) {
+		return "settingsaccount";
+	}
+	
+	@PostMapping("/settingsaccount")
+	public String postLogin(@Valid SettingsAccountForm settingsAccountForm, BindingResult results) {
+		if(results.hasErrors())
+			return "settingsaccount";
+		return "myaccount";
 	}
 
 }
