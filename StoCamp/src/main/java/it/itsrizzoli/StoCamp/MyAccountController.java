b3ff3002a14b1ec2830c60ed8231480ebbfcf5a1
acriscuoli <a.criscuoli@itsrizzoli.it>
2020-04-20 12:41:59 +0200

    Modifiche al PostMapping di settingsaccount

--- StoCamp/src/main/java/it/itsrizzoli/StoCamp/MyAccountController.java
+++ StoCamp/src/main/java/it/itsrizzoli/StoCamp/MyAccountController.java
@@ -1,26 +1,49 @@
 package it.itsrizzoli.StoCamp;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyAccountController {
-	
-	
+	@Autowired
+	private UtenteDao userRepository;
 	@GetMapping("/settingsaccount")
 	public String modificaprofilo(SettingsAccountForm settingsAccountForm) {
+		
+	/*	ModelAndView mav = new ModelAndView();
+		
+		if (u != null) {
+			mav.setViewName("settingsccount");
+			mav.addObject("utente", u);
+		}
+		else {
+			mav.setViewName("redirect:/login");			
+		}    */
+		
 		return "settingsaccount";
 	}
 	
 	@PostMapping("/settingsaccount")
-	public String postModificaProfilo(@Valid SettingsAccountForm settingsAccountForm, BindingResult results) {
-		if(results.hasErrors())
-			return "settingsaccount";
-		return "redirect:/myaccount";
+	public String postModificaProfilo(@Valid SettingsAccountForm settingsAccountForm,
+			HttpSession session, @RequestParam("username") String username, 
+			@RequestParam("eta") int eta, @RequestParam("citta_residenza") String cittaresidenza,
+			@RequestParam("indirizzo") String indirizzo) {
+		
+		Utente u = (Utente)session.getAttribute("loggedUser");
+		u.setUsername(username);
+		u.setEta(eta);
+		u.setCittaresidenza(cittaresidenza);
+		u.setIndirizzo(indirizzo);
+	userRepository.save(u);
+	return "redirect:/myaccount";
 	}
 	
 	@GetMapping("/impostazioni")
